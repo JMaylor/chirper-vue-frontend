5527f71ed870d95902b94da6e3664ddf842ea7de
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-06-23 11:16:31 +0100

    add api url to env variable and load in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "/src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://chirperapi.herokuapp.com/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "/src"),
       },
     },
-  },
-});
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  });
+};
