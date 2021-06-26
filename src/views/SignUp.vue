59084361b4edacb4590ed0f1999758714bdcf1b0
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-06-26 19:09:07 +0100

    sign up form neatening up

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,10 +1,109 @@
 <template>
-  <form @submit.prevent="signUp" class="flex flex-col text-black">
-    <input disabled :value="user.email" />
-    <input v-model="name" placeholder="display name" />
-    <input v-model="handle" placeholder="handle" />
+  <form
+    @submit.prevent="signUp"
+    class="text-black space-y-2 flex flex-col mt-4"
+  >
+    <div class="flex items-center justify-center">
+      <label class="block w-24 text-gray-700 text-sm font-semibold">
+        Email
+      </label>
+      <input
+        class="
+          text-sm
+          appearance-none
+          rounded
+          w-64
+          py-2
+          px-3
+          text-gray-700
+          bg-gray-400
+          leading-tight
+          focus:outline-none
+          focus:shadow-outline
+          h-10
+        "
+        disabled
+        type="text"
+        :value="user.email"
+      />
+    </div>
+    <div class="flex items-center justify-center">
+      <label class="block w-24 text-gray-700 text-sm font-semibold">
+        Name
+      </label>
+      <input
+        class="
+          text-sm
+          appearance-none
+          rounded
+          w-64
+          py-2
+          px-3
+          text-gray-700
+          bg-gray-200
+          leading-tight
+          focus:outline-none
+          focus:shadow-outline
+          h-10
+        "
+        type="text"
+        pattern="[a-zA-Z0-9 ]{3,20}"
+        required
+        v-model="name"
+      />
+    </div>
+    <div class="flex items-center justify-center">
+      <label class="block w-24 text-gray-700 text-sm font-semibold" for="email">
+        Handle
+      </label>
+      <div class="flex focus-within:ring">
+        <div
+          class="
+            h-10
+            w-8
+            rounded-l
+            bg-gray-400
+            items-center
+            justify-center
+            flex
+          "
+        >
+          @
+        </div>
+        <input
+          class="
+            text-sm
+            rounded-r
+            w-56
+            py-2
+            px-3
+            text-gray-700
+            bg-gray-200
+            focus:outline-none
+            h-10
+          "
+          pattern="[A-Za-z0-9]{6,20}"
+          required
+          v-model="handle"
+        />
+      </div>
+    </div>
+
     <img :src="user.picture" alt="user.name" class="rounded-full w-16" />
-    <button type="submit">sign up</button>
+    <button
+      type="submit"
+      class="
+        bg-black
+        text-white
+        dark:text-black
+        dark:bg-white
+        w-16
+        mx-auto
+        rounded
+      "
+    >
+      sign up
+    </button>
   </form>
 </template>
 
