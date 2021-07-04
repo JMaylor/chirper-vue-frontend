d2c0c168396e3419bea78aac89b6da5e63075fac
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-07-04 22:49:23 +0100

    change pink to rose

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "media",
@@ -8,6 +10,9 @@ module.exports = {
       },
       transitionProperty: {
         fill: "fill",
+      },
+      colors: {
+        pink: colors.rose,
       },
     },
   },
