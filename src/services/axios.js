a45c9cdc8ea079f1883a92d9718f06a8ca66a815
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-09-01 15:35:06 +0100

    axios instance creation

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 import store from "../store";
 
-axios.interceptors.request.use(async config => {
+const instance = axios.create({
+  baseURL: '/api/',
+  timeout: 30000
+})
+
+instance.interceptors.request.use(async config => {
   const token = await store.state.auth.getTokenSilently();
   config.headers.Authorization = `Bearer ${token}`
   return config
 })
 
-export default axios
+export default instance
