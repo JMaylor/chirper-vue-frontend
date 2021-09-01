b1eae96121b153a8add34a2e57c8eb4824a9cf27
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-09-01 15:35:19 +0100

    script setup refactor

--- src/components/feed/ChirpComments.vue
+++ src/components/feed/ChirpComments.vue
@@ -9,8 +9,8 @@
     />
     <div class="space-y-2 flex-grow">
       <div class="flex space-x-2 items-center whitespace-nowrap">
-        <span class="font-bold">{{ user.user_name }}</span
-        ><span class="text-gray-500">@{{ user.handle }}</span>
+        <span class="font-bold">{{ user.user_name }}</span>
+        <span class="text-gray-500">@{{ user.handle }}</span>
       </div>
       <form @submit.prevent="postComment" class="flex space-x-2">
         <textarea
@@ -21,21 +21,11 @@
           v-model="text"
           placeholder="What do you make of this?"
           rows="2"
-          class="
-            p-2
-            w-full
-            rounded-lg
-            bg-transparent
-            resize-none
-            focus:outline-none
-            focus:ring
-          "
+          class="p-2 w-full rounded-lg bg-transparent resize-none focus:outline-none focus:ring"
         ></textarea>
         <div class="flex flex-col items-center text-sm justify-between">
           <span :class="textColorClass">{{ commentLength }}/140</span>
-          <button type="submit" class="bg-blue-500 px-3 py-1 rounded-full">
-            Comment
-          </button>
+          <button type="submit" class="bg-blue-500 px-3 py-1 rounded-full">Comment</button>
         </div>
       </form>
     </div>
@@ -44,12 +34,12 @@
   <div
     class="flex w-full space-x-2"
     v-for="{
-      body,
-      comment_id,
-      created_at,
-      handle,
-      picture,
-      user_name,
+  body,
+    comment_id,
+    created_at,
+    handle,
+    picture,
+    user_name,
     } in comments"
     :key="comment_id"
   >
@@ -65,11 +55,11 @@
         <router-link
           class="rounded-full group focus:outline-none"
           :to="{ name: 'User', params: { handle: handle } }"
-          ><span
-            class="font-bold mr-2 group-hover:underline group-focus:underline"
-            >{{ user_name }}</span
-          ><span class="text-gray-500">@{{ handle }}</span></router-link
-        ><span class="text-gray-500">•</span>
+        >
+          <span class="font-bold mr-2 group-hover:underline group-focus:underline">{{ user_name }}</span>
+          <span class="text-gray-500">@{{ handle }}</span>
+        </router-link>
+        <span class="text-gray-500">•</span>
         <span class="text-sm text-gray-500">{{ time(created_at) }}</span>
       </div>
       {{ body }}
@@ -78,95 +68,82 @@
   <button v-if="!allCommentsLoaded" @click="fetchComments">load more</button>
 </template>
 
-<script>
-  import axios from "axios";
-  import TimeAgo from "javascript-time-ago";
-  import en from "javascript-time-ago/locale/en";
-  TimeAgo.locale(en);
-  const timeAgo = new TimeAgo("en-GB");
+<script setup>
+import { useStore } from "vuex";
+import { ref, inject, onMounted, computed } from "vue";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+TimeAgo.locale(en);
+const timeAgo = new TimeAgo("en-GB");
 
-  export default {
-    name: "ChirpComments",
-    props: {
-      chirpId: Number,
-    },
-    data() {
-      return {
-        comments: [],
-        text: "",
-        allCommentsLoaded: false,
-      };
-    },
-    computed: {
-      user() {
-        return this.$store.state.user;
+const emit = defineEmits(["increment"]);
+
+const props = defineProps({
+  chirpId: Number,
+});
+
+const axios = inject("axios");
+
+const comments = ref([]);
+
+const allCommentsLoaded = ref(false);
+
+const oldestTimestamp = computed(() => {
+  const numComments = comments.value.length;
+  if (numComments == 0) return 0;
+  return comments.value[numComments - 1].created_at;
+});
+
+async function fetchComments() {
+  try {
+    const { data } = await axios.get(`comments`, {
+      params: {
+        chirpId: props.chirpId,
+        timestamp: oldestTimestamp.value,
       },
-      commentLength() {
-        return this.text.length;
-      },
-      textColorClass() {
-        if (this.commentLength == 140) return "text-red-500";
-        if (this.commentLength >= 120) return "text-yellow-500";
-        return "";
-      },
-      oldestTimestamp() {
-        const numComments = this.comments.length;
-        if (numComments == 0) return 0;
-        return this.comments[numComments - 1].created_at;
-      },
-    },
-    methods: {
-      async fetchComments() {
-        try {
-          const token = await this.$auth.getTokenSilently();
-          const { data: comments } = await axios.get(`/api/comments`, {
-            params: {
-              chirpId: this.chirpId,
-              timestamp: this.oldestTimestamp,
-            },
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          this.comments.push(...comments);
-          if (comments.length < 3) {
-            this.allCommentsLoaded = true;
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      },
-      async postComment($event) {
-        console.log($event);
-        try {
-          const token = await this.$auth.getTokenSilently();
-          const { data: comment } = await axios.post(
-            `/api/comment`,
-            {
-              chirp_id: this.chirpId,
-              body: this.text,
-            },
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
-          this.comments.unshift({
-            ...comment,
-            ...this.user,
-          });
-          this.$emit("increment");
-        } catch (error) {
-          console.log(error);
-        }
-      },
-      time(created_at) {
-        return timeAgo.format(new Date(created_at), "twitter");
-      },
-    },
-    mounted() {
-      this.fetchComments();
-    },
-  };
+    });
+    comments.value.push(...data);
+    if (data.length < 3) {
+      allCommentsLoaded.value = true;
+    }
+  } catch (error) {
+    alert(error);
+  }
+}
+onMounted(() => fetchComments());
+
+function time(created_at) {
+  return timeAgo.format(new Date(created_at), "twitter");
+}
+
+const commentLength = computed(() => {
+  return text.value.length;
+});
+const textColorClass = computed(() => {
+  if (commentLength.value == 140) return "text-red-500";
+  if (commentLength.value >= 120) return "text-yellow-500";
+  return "";
+});
+
+const text = ref("");
+const user = computed(() => {
+  const store = useStore();
+  return store.state.user;
+});
+
+async function postComment() {
+  try {
+    const { data: comment } = await axios.post(`comment`, {
+      chirp_id: props.chirpId,
+      body: text.value,
+    });
+    comments.value.unshift({
+      ...comment,
+      ...user.value,
+    });
+    emit("increment");
+  } catch (error) {
+    alert(error);
+  }
+}
 </script>
