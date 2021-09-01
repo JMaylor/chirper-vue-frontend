c3338ffaa79bb0d9ed9a0ec9e711b6606297a894
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-09-01 15:41:09 +0100

    script setup refactor

--- src/components/feed/ChirpAction.vue
+++ src/components/feed/ChirpAction.vue
@@ -1,23 +1,7 @@
 <template>
   <button @click="toggleLike" class="action-container" :class="buttonClasses">
     <div
-      class="
-        h-8
-        w-8
-        flex
-        relative
-        rounded-full
-        items-center
-        justify-center
-        transition
-        duration-200
-        bg-opacity-0
-        group-hover:bg-opacity-10
-        dark:group-hover:bg-opacity-30
-        group-focus-visible:bg-opacity-10
-        dark:group-focus-visible:bg-opacity-30
-        group-focus-visible:ring-2
-      "
+      class="h-8 w-8 flex relative rounded-full items-center justify-center transition duration-200 bg-opacity-0 group-hover:bg-opacity-10 dark:group-hover:bg-opacity-30 group-focus-visible:bg-opacity-10 dark:group-focus-visible:bg-opacity-30 group-focus-visible:ring-2"
       :class="svgContainerClasses"
     >
       <svg
@@ -30,54 +14,37 @@
         <slot name="path" />
       </svg>
       <div
-        class="
-          bg-gray-500
-          absolute
-          hidden
-          px-2
-          py-1
-          max-w-6xl
-          group-focus-visible:block
-          -bottom-7
-          left-1/2
-          transform
-          transition
-          duration-200
-          -translate-x-1/2
-          rounded
-          text-xs text-white
-        "
+        class="bg-gray-500 absolute hidden px-2 py-1 max-w-6xl group-focus-visible:block -bottom-7 left-1/2 transform transition duration-200 -translate-x-1/2 rounded text-xs text-white"
       >
         <slot name="tooltip">Tooltip</slot>
       </div>
     </div>
-    <span class="text-sm transition duration-200"><slot /></span>
+    <span class="text-sm transition duration-200">
+      <slot />
+    </span>
   </button>
 </template>
 
-<script>
-  export default {
-    name: "ChirpAction",
-    props: {
-      buttonClasses: {
-        type: [Object, String],
-        required: true,
-      },
-      fill: {
-        type: [Object, String],
-        required: false,
-        default: "none",
-      },
-      svgContainerClasses: {
-        type: [Object, String],
-        required: true,
-      },
-    },
-  };
+<script setup>
+defineProps({
+  buttonClasses: {
+    type: [Object, String],
+    required: true,
+  },
+  fill: {
+    type: [Object, String],
+    required: false,
+    default: "none",
+  },
+  svgContainerClasses: {
+    type: [Object, String],
+    required: true,
+  },
+});
 </script>
 
 <style>
-  .action-container {
-    @apply flex items-center group space-x-2 px-1 focus:outline-none;
-  }
+.action-container {
+  @apply flex items-center group space-x-2 px-1 focus:outline-none;
+}
 </style>
