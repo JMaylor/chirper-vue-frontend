db35851333f7990a25c944dc4db80c523b192678
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-09-01 18:02:42 +0100

    script setup refactor

--- src/views/Compose.vue
+++ src/views/Compose.vue
@@ -7,66 +7,44 @@
       v-model="text"
       placeholder="What's happening?"
       rows="4"
-      class="
-        p-2
-        text-xl
-        w-full
-        rounded-lg
-        bg-transparent
-        resize-none
-        focus:outline-none
-        focus:ring
-      "
+      class="p-2 text-xl w-full rounded-lg bg-transparent resize-none focus:outline-none focus:ring"
     ></textarea>
     <div class="flex justify-between items-center">
-      <button type="submit" class="bg-blue-500 px-4 py-2 rounded-full">
-        Chirp
-      </button>
+      <button type="submit" class="bg-blue-500 px-4 py-2 rounded-full">Chirp</button>
       <span :class="textColorClass">{{ chirpLength }}/140</span>
     </div>
   </form>
 </template>
 
-<script>
-  import axios from "axios";
-  export default {
-    name: "Compose",
-    data() {
-      return {
-        text: "",
-      };
-    },
-    methods: {
-      async submitChirp() {
-        try {
-          const token = await this.$auth.getTokenSilently();
-          await axios.post(
-            "/api/chirps",
-            {
-              body: this.text,
-            },
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
-          this.$router.push({ name: "Home" });
-        } catch (error) {
-          alert(error);
-          console.error(error);
-        }
+<script setup>
+import { ref, inject, computed } from 'vue'
+import { useRouter } from 'vue-router'
+
+const axios = inject('axios')
+const router = useRouter()
+
+const text = ref('')
+
+const chirpLength = computed(() => text.value.length)
+
+const textColorClass = computed(() => {
+  if (chirpLength.value == 140) return "text-red-500";
+  if (chirpLength.value >= 120) return "text-yellow-500";
+  return "";
+})
+
+async function submitChirp() {
+  try {
+    await axios.post(
+      "chirps",
+      {
+        body: text.value,
       },
-    },
-    computed: {
-      chirpLength() {
-        return this.text.length;
-      },
-      textColorClass() {
-        if (this.chirpLength == 140) return "text-red-500";
-        if (this.chirpLength >= 120) return "text-yellow-500";
-        return "";
-      },
-    },
-  };
+    );
+    
+    router.push({ name: "Home" });
+  } catch (error) {
+    alert(error);
+  }
+}
 </script>
