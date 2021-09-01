31ba549114f8bb07217ab879b4eff9ec1a27da94
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-09-01 18:55:37 +0100

    script setup refactor

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -5,62 +5,58 @@
       @complete="searchHashtags"
       :suggestions="hashtags"
       @item-select="select"
-    >
-    </AutoComplete>
+    ></AutoComplete>
   </div>
   <router-view />
 </template>
 
-<script>
-  import axios from "axios";
-  import debounce from "lodash.debounce";
+<script setup>
+import { ref, inject } from 'vue'
+import { useRouter } from 'vue-router'
+import { useDebounceFn } from '@vueuse/core'
 
-  export default {
-    name: "Search",
-    data() {
-      return {
-        query: "",
-        hashtags: [],
-      };
-    },
-    methods: {
-      searchHashtags: debounce(async function () {
-        if (this.query == "") return (this.hashtags = []);
-        const { data: hashtags } = await axios.get(
-          `/api/hashtags/search/${this.query}`
-        );
-        this.hashtags = hashtags.map(({ tag }) => tag);
-      }, 200),
-      select({ value }) {
-        this.$router.push(`/hashtag/${value}`);
-      },
-    },
-  };
+const axios = inject('axios');
+
+const query = ref('')
+const hashtags = ref([])
+
+const searchHashtags = useDebounceFn(async () => {
+  if (query.value == "") return (hashtags.value = []);
+  const { data } = await axios.get(
+    `hashtags/search/${query.value}`
+  );
+  hashtags.value = data.map(({ tag }) => tag);
+}, 200)
+
+const router = useRouter()
+function select({ value }) {
+  router.push(`/hashtag/${value}`)
+}
 </script>
 
 <style>
-  /* https://primefaces.org/primevue/showcase/#/autocomplete */
-  .p-autocomplete {
-    @apply w-full;
-  }
+/* https://primefaces.org/primevue/showcase/#/autocomplete */
+.p-autocomplete {
+  @apply w-full;
+}
 
-  .p-autocomplete-input {
-    @apply bg-gray-300 dark:bg-gray-800 rounded-full focus:outline-none py-2 px-3 w-full;
-  }
+.p-autocomplete-input {
+  @apply bg-gray-300 dark:bg-gray-800 rounded-full focus:outline-none py-2 px-3 w-full;
+}
 
-  .p-autocomplete-panel {
-    @apply bg-gray-200 dark:bg-gray-800 text-black dark:text-white shadow-lg;
-  }
+.p-autocomplete-panel {
+  @apply bg-gray-200 dark:bg-gray-800 text-black dark:text-white shadow-lg;
+}
 
-  .p-autocomplete-items {
-    @apply divide-y divide-gray-500;
-  }
+.p-autocomplete-items {
+  @apply divide-y divide-gray-500;
+}
 
-  .p-autocomplete-item {
-    @apply p-3 hover:bg-gray-500 hover:text-white;
-  }
+.p-autocomplete-item {
+  @apply p-3 hover:bg-gray-500 hover:text-white;
+}
 
-  .p-highlight {
-    @apply bg-gray-500 text-white;
-  }
+.p-highlight {
+  @apply bg-gray-500 text-white;
+}
 </style>
